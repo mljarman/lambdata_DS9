1b115dd3057d8f4b5a28dc5cda7121cf18ff53c8
mljarman <mljarman@users.noreply.github.com>
2019-12-03 09:16:58 -0700

    add utility functions

---
+++ lambdata_mljarman/__init__.py
@@ -0,0 +1,14 @@
+"""lambdata is a collection of data science helper functions"""
+
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+# sample data:
+ONES = pd.DataFrame(np.ones(10))
+ZEROS = pd.DataFrame(np.zeros(50))
+
+
+# sample functions:
+def increment(x):
+    return (x + 1)
---
+++ lambdata_mljarman/df_utils.py
@@ -0,0 +1,4 @@
+"""
+utility functions for working specifically with DataFrames
+"""
+TEST_DF = pandas.DataFrame([1, 3, 4, 5, 6])
