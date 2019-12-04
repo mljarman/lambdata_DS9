3583344e105ecead2b60f259569ea4b9d83c7632
mljarman <mljarman@users.noreply.github.com>
2019-12-04 15:56:25 -0700

    remove unused code

--- lambdata_mljarman/for_models.py
+++ lambdata_mljarman/for_models.py
@@ -4,23 +4,13 @@ Utility functions for preparing to run models on data.
 import pandas as pd
 import numpy as np
 
-#
-# def set_splits(df, train_percent=.6, val_percent=.2, test_percent=.2,
-#                Random_State=None):
-#     """
-#     function to split data into train, test, val sets.
-#     """
-#     np.random.seed(Random_State)
-#     percent = train_percent + val_percent
-#     train, val, test = np.split(df.sample(frac=1), [int(train_percent*len(df)),
-#                                                     int(percent*len(df))])
-#     return train, val, test
 
 class Model_Prep:
     """
     Preparing dataset to run predictive models.
     """
-    def __init__(self, df, train_percent, val_percent, test_percent, random_state):
+    def __init__(self, df, train_percent =.6, val_percent =.2, test_percent =.2,
+                random_state = 42):
         self.df = df
         self.train_percent = train_percent
         self.val_percent = val_percent
@@ -33,6 +23,6 @@ class Model_Prep:
         """
         np.random.seed(self.random_state)
         percent = self.train_percent + self.val_percent
-        train, val, test = np.split(df.sample(frac=1), [int(self.train_percent*len(self.df)),
-                                                        int(percent*len(df))])
+        train, val, test = np.split(df.sample(frac=1), [int(self.train_percent*
+                                                        len(self.df)),int(percent*len(df))])
         return train, val, test
