ef19eb729d20af2c1b623bfaf9ef4514321a2dcb
mljarman <mljarman@users.noreply.github.com>
2019-12-04 11:05:24 -0700

    change version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
     setuptools.setup(
     name="lambdata-mljarman",
-    version = "0.1.3",
+    version = "0.1.4",
     author = "mljarman",
     description = "a collection of data science helper functions",
     long_description = LONG_DESCRIPTION,
